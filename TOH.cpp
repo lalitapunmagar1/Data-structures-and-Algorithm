9bb8f1fb8b449bde11e262de8c6fb1a25b30c0bb
lalitapunmagar1 <145996502+lalitapunmagar1@users.noreply.github.com>
2024-02-09 17:17:20 +0545

    Add files via upload

---
+++ TOH.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+void TOH(int n, char sour, char temp, char des)
+{
+	int steps=0;
+    if (n == 1) {
+        cout << "Move Disk " << n << " from " << sour << " to " << des << endl;
+        return;
+    }
+
+    TOH(n - 1, sour, des, temp);
+    cout << "Move Disk " << n << " from " << sour << " to " << des << endl;
+    TOH(n - 1, temp, sour, des);
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter no. of disks:";
+    cin >> n;
+    int steps=pow(2,n)-1;
+    cout<< "for " <<n<< " disks we have " <<steps<< " steps. " <<endl;
+    TOH(n, 'A', 'B', 'C');
+
+    return 0;
+}
---
+++ fibonacci.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int fibo(int n) {
+   if((n==1)||(n==0)) {
+      return(n);
+   }else {
+      return(fibo(n-1)+fibo(n-2));
+   }
+}
+int main() {
+   int n , i=0;
+   cout << "Enter the number of terms of series : ";
+   cin >> n;
+   cout << "\nFibonnaci Series : ";
+   while(i < n) {
+      cout << " " << fibo(i);
+      i++;
+   }
+   return 0;
+}

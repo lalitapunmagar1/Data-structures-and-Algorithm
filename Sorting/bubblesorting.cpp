6c96a1a2038f2917ed2ef2d1c08ce738a1187c5c
lalitapunmagar1 <145996502+lalitapunmagar1@users.noreply.github.com>
2024-03-28 14:33:00 +0545

    Add files via upload

---
+++ Sorting/bubblesorting.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n,i,j;
+	cout<<"Enter the size of n ";
+	cin>>n;
+	int arr[n];
+	for( i = 0; i<n; i++ ){
+		cout<<"Enter the element" <<i+1 << ": ";
+		cin>>arr[i];
+	}
+	for (i = 0; i < n - 1; i++){
+		for (j = 0; j < n - i - 1; j++){
+		    if (arr[j] > arr[j + 1])
+            swap(arr[j], arr[j + 1]);
+        }
+	}
+			
+	cout<<"Sorted array :"<<endl;
+	for( i = 0; i<n; i++ ){
+		cout<< arr[i] <<" ";
+	}
+		
+}
